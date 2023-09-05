6194d13f6d735925b3226b1b4d47fd741db509cb
ArseneBrosy <arsene.brosy@divtec.ch>
2023-09-05 15:29:06 +0200

    html

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 /**
  * Fichier de base pour application web HTML/CSS/JS
- * @author  Steve Fallet
- * @version 0.2
+ * @author  Arsène Brosy
+ * @version 0.3
  * @since   2019-08-19
  */
 'use strict'; // Demande un interprétation stricte du code
 
-let celsius = parseInt(prompt("Température en celsius"));
+/*let celsius = parseFloat(prompt("Température en celsius"));
 if (isNaN(celsius)) {
   alert("Entrez un nombre");
 } else {
   let fahrenheit = celsius * 9 / 5 + 32;
   alert(`${celsius}°C = ${fahrenheit}°F`);
-}
+}*/
