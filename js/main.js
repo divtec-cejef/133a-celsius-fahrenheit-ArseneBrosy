f18a52aa9f3894df3430fb1867929bc802345182
ArseneBrosy <arsene.brosy@divtec.ch>
2023-09-05 15:38:57 +0200

    new branch

--- js/main.js
+++ js/main.js
@@ -13,3 +13,28 @@ if (isNaN(celsius)) {
   let fahrenheit = celsius * 9 / 5 + 32;
   alert(`${celsius}°C = ${fahrenheit}°F`);
 }*/
+
+document.querySelector("#button-convert").addEventListener("click", convert);
+document.addEventListener("keydown", (e) => {
+  if (e.code === "Enter") {
+    convert();
+  }
+})
+
+function convert() {
+  // récuperer la valeur de l'input
+  let celsius = parseFloat(document.querySelector("#input-celsius").value);
+
+  // verifier que c'est un nombre
+  if (isNaN(celsius)) {
+    alert("Entrez un nombre");
+    return;
+  }
+
+  // convertir en fahrenheit et l'afficher
+  let fahrenheit = celsius * 9 / 5 + 32;
+  document.querySelector("#result").innerHTML = "${fahrenheit}°F";
+
+  // inscrire le résultat dans l'historique
+  document.querySelector("#history-list").innerHTML += "<li>${celsius}°C = ${fahrenheit}°F</li>";
+}
